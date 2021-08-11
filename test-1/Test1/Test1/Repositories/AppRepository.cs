438e63a3bf2303a3a40bd24bfde2ed46b2c6a3d9
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-08-11 22:09:54 +0700

    add base helper, repos

---
+++ test-1/Test1/Test1/Repositories/AppRepository.cs
@@ -0,0 +1,43 @@
+using System;
+using Test1.Helpers;
+using Test1.Models;
+
+namespace Test1.Repositories
+{
+    class AppRepository
+    {
+        private static AppRepository instance = null;
+        private AppRepository(){}
+
+		//data-model
+		// List<Model> listModels = new List<Model>()
+
+
+        public static AppRepository getIntance()
+        {
+            if (AppRepository.instance == null)
+            {
+                AppRepository.instance = new AppRepository();
+            }
+            return AppRepository.instance;
+        }
+
+		// public List<Model> getListModels(){
+		// 	DataTable result = DatabaseHelper.query("SELECT * FROM Model");
+		// 	List<Model> listModels = new List<Model>();
+		// 	foreach (DataRow row in result.Rows)
+        //     {
+		// 		Model model = new Model();
+        //         model.setId(row["Id"]);
+        //         model.setName(row["Name"].ToString());
+		// 		listModels.add(model);
+        //     }
+		// 	return listModels;
+		// }
+
+		// public void addModel(String name, String Id){
+		// 	String sqlCommand = $"INSERT INTO Model Values({Id}, {name}";
+		// 	DatabaseHelper.execute(sqlCommand);
+		// }
+    }
+}
