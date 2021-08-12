ec8edb02e401d4cb50f0c2a1578df6960228d385
maiquockhanh <mqkhanh19@apcs.vn>
2021-08-12 15:48:12 +0700

    add cats

--- TestApp/CountryItem.cs
+++ TestApp/CountryItem.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TestApp.Models;
 
 namespace TestApp
 {
@@ -29,6 +30,26 @@ namespace TestApp
         public Button GetButton()
         {
             return buttonCountry;
+        }
+
+        public void AddCategories(string name)
+        {
+            Button btn_add = new Button();
+            btn_add.Text = name;
+            btn_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            btn_add.Dock = System.Windows.Forms.DockStyle.Top;
+            btn_add.FlatAppearance.BorderSize = 0;
+            btn_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btn_add.ForeColor = System.Drawing.Color.LightGray;
+            btn_add.Location = new System.Drawing.Point(0, 135);
+            btn_add.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            btn_add.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
+            btn_add.Size = new System.Drawing.Size(194, 45);
+            btn_add.TabIndex = 3;
+            btn_add.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            btn_add.UseVisualStyleBackColor = false;
+            //btn_add.Click += new EventHandler(this.button_Click);
+            panelCountry.Controls.Add(btn_add);
         }
 
     }
