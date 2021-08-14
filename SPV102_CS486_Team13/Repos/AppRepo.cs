a70ac895238b892a1dc7fd6c29cb59f4dc028d3e
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-08-14 09:04:35 +0700

    add repo

--- SPV102_CS486_Team13/Repos/AppRepo.cs
+++ SPV102_CS486_Team13/Repos/AppRepo.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SPV102_CS486_Team13.Helpers;
+using System.Data;
 
 namespace SPV102_CS486_Team13.Repos
 {
     class AppRepo
     {
+        private static AppRepo instance = null;
+        private static Boolean isDev = true;
+        private AppRepo() { }
+
+        public static AppRepo getInstnace()
+        {
+            if (AppRepo.instance == null)
+            {
+                AppRepo.instance = new AppRepo();
+            }
+            return AppRepo.instance;
+        }
+
+        public List<string> getAllContestant()
+        {
+            if (isDev)
+            {
+
+            }
+
+            List<String> list = new List<string>();
+            String sqlString = $"SELECT * FROM";
+            DataTable result = DatabaseHelper.query(sqlString);
+            foreach(DataRow row in result.Rows)
+            {
+
+            }
+            return list;
+        }
+
+        public List<string> getAllExaminers()
+        {
+            if (isDev)
+            {
+
+            }
+
+            List<String> list = new List<string>();
+            String sqlString = $"SELECT * FROM";
+            DataTable result = DatabaseHelper.query(sqlString);
+            foreach(DataRow row in result.Rows)
+            {
+
+            }
+            return list;
+        }
+
+        public List<string> getAllRounds()
+        {
+            if (isDev)
+            {
+
+            }
+
+            List<String> list = new List<string>();
+            String sqlString = $"SELECT * FROM";
+            DataTable result = DatabaseHelper.query(sqlString);
+            foreach(DataRow row in result.Rows)
+            {
+
+            }
+            return list;
+        }
+
+
     }
 }
