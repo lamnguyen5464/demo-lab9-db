7182cecfd5d3978b3076faaa59afbd0a5e90ebd9
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-08-14 08:56:38 +0700

    add gitignore and helper

---
+++ SPV102_CS486_Team13/Repos/AppRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPV102_CS486_Team13.Repos
+{
+    class AppRepo
+    {
+    }
+}
