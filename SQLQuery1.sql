9dd5e14b9a99577e53aa85a0741c21b795ec4bb4
thanhdat1902 <thanhdat19022001@gmail.com>
2021-08-14 10:18:44 +0700

    update sql 01

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,7 +3,7 @@ go
 use SuperVoice;
 
 create table Contestants (
-	Id int primary key identity(1,1),
+	id int primary key identity(1,1),
 	name nvarchar(50),
 	birthday date,
 	university nvarchar(50),
@@ -11,7 +11,7 @@ create table Contestants (
 )
 
 create table Examiners (
-	Id int primary key identity(1,1),
+	id int primary key identity(1,1),
 	name nvarchar(50),
 	birthday date,
 	moreInfo text,
@@ -25,16 +25,16 @@ create table Rounds (
 )
 
 create table Results(
-	ID int primary key identity(1,1),
-	ContestantID int,
-	ExaminerID int,
-	RoundID int,
+	id int primary key identity(1,1),
+	contestantID int,
+	examinerID int,
+	roundID int,
 	feedback text,
 	score int check (score >= 0 AND score <=100),
-	Role nvarchar(50) --check (Role in ('official', 'reverse'))	
+	role nvarchar(50) --check (Role in ('official', 'reverse'))	
 	foreign key (ContestantID) references Contestants(Id),
 	foreign key (ExaminerID) references Examiners(Id),
-	foreign key (RoundID) references Rounds(ID),
+	foreign key (RoundID) references Rounds(Id),
 )
 ----------------------------------- INSERTION
 -- Contestants
@@ -56,10 +56,10 @@ values
 -- Rounds
 insert into Rounds(name,numberReserve,numberOfficial)
 values
-(N'',32,6),
-(N'',16,3),
-(N'',8,2),
-(N'',4,1)
+(N'Audition',32,6),
+(N'2vs2',16,3),
+(N'Semi Final',8,2),
+(N'Final',4,1)
 
 go
 insert into Results(ContestantID,ExaminerID,RoundID,Role,Feedback,Score)
@@ -87,37 +87,80 @@ values
 --begin
 
 
------------------------------------ SELECT SECTION 
----- getListContestants
---go
---create procedure sp_getListContestants 
---as 
---begin
---begin transaction
---set transaction isolation level read committed
---	select * from Contestants
---end
+--------------------------------- SELECT SECTION 
+-- getListContestants
+go
+create procedure sp_getListContestants 
+as 
+begin
+begin transaction
+set transaction isolation level read committed
+	select * from Contestants
+	commit transaction;
+end
+-- EXEC
+exec sp_getListContestants
 
----- EXEC
+-- getContestantsByRoundId
+go
+create procedure sp_getContestantsByRoundId @roundId int
+as 
+begin
+begin transaction
+set transaction isolation level read committed
+	select c.Id, c.name, c.birthday, c.university, avg(r.score) as 'averageScore'
+	from Contestants c
+	join Results r on c.Id = r.ContestantID
+	where r.RoundID = @roundId
+	group by c.Id,c.name, c.birthday, c.university
+	commit transaction;
+end
+-- EXEC
+exec sp_getContestantsByRoundId 1
 
----- getListExaminers
---go 
---create procedure sp_getListExaminers
---as
---begin transaction
---set transaction isolation level read committed
+-- getListExaminers
+go 
+create procedure sp_getListExaminers
+as
+begin 
+begin transaction
+set transaction isolation level read committed
+	select * from Examiners
+	commit transaction;
+end
+-- EXEC
+exec sp_getListExaminers 
 
----- getResultByContestantId
+-- getListRounds
+go 
+create procedure sp_getListRounds
+as
+begin 
+begin transaction
+set transaction isolation level read committed
+	select * from Rounds
+	commit transaction;
+end
+
+-- EXEC
+exec sp_getListRounds
+
+
+
+
+-- getResultByContestantId
 --go 
 --create procedure sp_getResultsByContestantId
 --as
+--begin
 --begin transaction
 --set transaction isolation level read committed
 
-
+--end
 -- 
 
 ----------------------------------- NOSELECT SECTION
+
 
 use master
 drop database SuperVoice
